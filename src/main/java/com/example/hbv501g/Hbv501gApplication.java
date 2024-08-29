8398c1c1e3a75af535e656248f96a66ea18ef38d
lumeto-de-diamanto <gretar.vidir@gmail.com>
2024-08-29 10:43:01 +0000

    Initial commit

---
+++ src/main/java/com/example/hbv501g/Hbv501gApplication.java
@@ -0,0 +1,14 @@
+package com.example.hbv501g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hbv501gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hbv501gApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
