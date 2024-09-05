0f5bf23b75dd00d8a8aa8bf6fe9a85a881c27c0f
arnor9 <arg82@hi.is>
2024-09-05 09:41:16 +0000

    comment Arnór

--- src/main/java/com/example/hbv501g/Hbv501gApplication.java
+++ src/main/java/com/example/hbv501g/Hbv501gApplication.java
@@ -8,7 +8,7 @@ public class Hbv501gApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Hbv501gApplication.class, args);
-		//comment frá Arnóri
+		//comment Arnór
 		System.out.println("Hello world");
 	}
 
